f164fd18633fe654e2b6d4d2fe0c95582edfdbbd
jschaik <9020512@student.zadkine.nl>
2022-10-13 13:26:07 +0200

    js 5.1

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("hello world");
+var getal1 = 12
+var getal2 = 4;
+var kleur = prompt ("geef een engelse kleurnaam: (red of blue","");
+// kleur aanpassen 
+// document.getelementbyid("titel").style.color = "red";
+document.getElementById("yunishaway").style.color = kleur;
+//tekst aanpassen 
+document. getElementById("p1").innerhtml= "hello"+kleur+"!"
